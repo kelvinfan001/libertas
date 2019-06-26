77b9d655669c1882e8b1886548a96154099afd6e
Kelvin Fan <kelvinfan001@gmail.com>
2019-06-26 17:00:19 -0400

    refactored app and fixed bug in enrollAdminTestProg

--- app/javascript/enrollAdminTestProg.js
+++ app/javascript/enrollAdminTestProg.js
@@ -6,7 +6,7 @@
 
 'use strict';
 
-const enrollAdmin = require('./enrollAdmin.js');
+const enrollAdminModule = require('./enrollAdmin.js');
 const path = require('path');
 
 const ccpPath = path.resolve(__dirname, '..', '..', 'libertas-dev-network', 'connection-sipher.json');
@@ -15,7 +15,7 @@ const networkDirPath = path.resolve(__dirname, '..', '..', 'libertas-dev-network
 async function main() {
 
     try {
-        enrollAdmin.enroll(ccpPath, 'ca.libertas.sipher.co', networkDirPath, path.join(process.cwd(), 'wallet'), 'admin', 'adminpw', 'SipherMSP');
+        enrollAdminModule.enrollAdmin(ccpPath, 'ca.libertas.sipher.co', networkDirPath, path.join(process.cwd(), 'wallet'), 'admin', 'adminpw', 'SipherMSP');
     } catch (error) {
         console.error(`${error}`);
         process.exit(1);
