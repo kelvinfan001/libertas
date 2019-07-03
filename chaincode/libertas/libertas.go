285336e5aa90e7951c9e94cdf4ca195444e38be5
Kelvin Fan <kelvinfan001@gmail.com>
2019-07-03 01:14:32 -0400

    chaincode installable. However, cid dependency not working. commented out for now

--- chaincode/libertas/libertas.go
+++ chaincode/libertas/libertas.go
@@ -14,7 +14,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/hyperledger/fabric/core/chaincode/lib/cid"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
@@ -109,16 +108,17 @@ func (t *Libertas) CreateAccount(stub shim.ChaincodeStubInterface, args []string
 		return shim.Error("Incorrect number of arguments. Expecting 4.")
 	}
 
-	// Get the identity of the person calling this function.
-	id, err = cid.GetID(stub)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	// Check if caller is admin.
-	// TODO: SHOULD CHECK CERTIFICATE TO BE MORE RIGOROUS!
-	if id != "admin" {
-		return shim.Error("Cannot find admin credentials")
-	}
+	// TODO: THIS PART IS NOT WORKING. COMMENTING OUT FOR NOW. DEPENDENCY PROBELMS. VENDOR???
+	// // Get the identity of the person calling this function.
+	// id, err = cid.GetID(stub)
+	// if err != nil {
+	// 	return shim.Error(err.Error())
+	// }
+	// // Check if caller is admin.
+	// // TODO: SHOULD CHECK CERTIFICATE TO BE MORE RIGOROUS!
+	// if id != "admin" {
+	// 	return shim.Error("Cannot find admin credentials")
+	// }
 
 	// Get list of accounts from the ledger
 	accountsListBytes, err = stub.GetState("Accounts List")
