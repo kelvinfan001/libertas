3b11f7d12572a240444b47ee61be72fe69127318
Kelvin Fan <kelvinfan001@gmail.com>
2019-07-05 17:40:51 -0400

    added createAccount.js frontend endpoint

---
+++ app/javascript/createAccount.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 Sipher Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const path = require('path');
+
+async function createAccount(connectionProfilePath, walletPath, channelName, contractName, id, name, email, kind) {
+    try {
+        // Create a new file system based walllet for managing identities.
+        const wallet = new FileSystemWallet(walletPath);
+
+        // Check to see if admin credentials exist in wallet. This is only a precursory check. 
+        const adminExists = await wallet.exists('admin');
+        if (!adminExists) {
+            console.log('Admin credentials do not exist in the wallet.')
+            return;
+        }
+
+        // Create a new gateway for connecting to peer node.
+        const gateway = new Gateway();
+        await gateway.connect(connectionProfilePath, { wallet, identity: username, discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) that our contract is deployed to.
+        const network = await gateway.getNetwork(channelName);
+
+        // Get the contract from the network.
+        const contract = network.getContract(contractName);
+
+        // Submit the transaction.
+        await contract.submitTransaction('CreateAccount', id, name, email, kind);
+        console.log('Transaction has been submitted');
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
