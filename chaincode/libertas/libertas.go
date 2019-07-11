69217283657210608e76337bdacdbdb30e74a3cd
kaii789 <kailong.huang@mail.utoronto.ca>
2019-07-11 12:55:35 -0400

    no longer keeping track of voters in project

--- chaincode/libertas/libertas.go
+++ chaincode/libertas/libertas.go
@@ -72,14 +72,6 @@ func (t *Libertas) Init(stub shim.ChaincodeStubInterface) pb.Response {
 		return shim.Error(err.Error())
 	}
 
-	// Initialize voters list
-	votersList := VotersList{}
-	votersListBytes, _ := json.Marshal(accountsList)
-	err = stub.PutState("Voters List", accountsListBytes)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
 	return shim.Success(nil)
 }
 
