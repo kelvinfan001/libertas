dc44e398590dfba47fd10fc1dcd2717c8fecbbaa
Kelvin Fan <kelvinfan001@gmail.com>
2019-07-15 12:27:53 -0400

    added addAffiliation to test programs

---
+++ app/javascript/test_programs/addAffiliationTestProg.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2019 Sipher Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const addAffiliationModule = require('../addAffiliation.js');
+const path = require('path');
+
+const walletPath = path.join(process.cwd(), 'wallet')
+const ccpPath = path.resolve(__dirname, '..', '..', '..', 'libertas-dev-network', 'connection-sipher.json');
+
+async function main() {
+
+    try {
+        await addAffiliationModule.addAffiliation(walletPath, ccpPath, 'voting_district1')
+    } catch (error) {
+        console.error(`${error}`);
+        process.exit(1);
+    }
+}
+
+main()
