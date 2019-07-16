a66c2c9e251af1715b6d277c6cde156f2b2daab5
kaii789 <kailong.huang@mail.utoronto.ca>
2019-07-16 15:44:54 -0400

    initialize HTTP server for handling app api requests

---
+++ api-http-server/api-server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const accountsModule = require('../app/javascript/accounts')
+
+// Set paths to connection profile and wallet
+const ccpPath = path.resolve(__dirname, '..', 'libertas-dev-network', 'connection-sipher.json');
+const networkDirPath = path.resolve(__dirname, '..', 'libertas-dev-network')
+const walletPath = path.join(__dirname, 'app', 'javascript', 'wallet')
+
+app.post('/createAccount', (req, res) => {
+    // get params
+    var id = req.body.id;
+    var name = req.body.name;
+    var email = req.body.email;
+    var accountType = req.body.accountType;
+
+    accountsModule.createAccount(ccpPath, walletPath, "test", "libertas", id, name, email, accountType);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
+
+
+
+
+
+
+
+
