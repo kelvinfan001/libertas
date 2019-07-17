d8f3c8731130e494613e5c98688173efe8bfec8a
kaii789 <kailong.huang@mail.utoronto.ca>
2019-07-17 18:24:35 -0400

    add campaign related HTTP tests

--- api-http-server/test.js
+++ api-http-server/test.js
@@ -2,29 +2,52 @@
 
 const fetch = require('node-fetch');
 
-async function wrapper() {
-    // await fetch('http://155.138.134.91/createAccount', {
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //         id: 'jingleman',
-    //         name: 'Jingle Man',
-    //         email: 'jingle@sipher.co',
-    //         accountType: 'Personal'
-    //     }),
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json',
-    //     }
-    // });
+//---------------------------------------ACCOUNT TESTS------------------------------------------------
+await fetch('http://155.138.134.91/createAccount', {
+    method: 'POST',
+    body: JSON.stringify({
+        id: 'jingleman',
+        name: 'Jingle Man',
+        email: 'jingle@sipher.co',
+        accountType: 'Personal'
+    }),
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+});
 
-    let url = 'http://155.138.134.91/queryAccountByID?idToQuery=jingleman';
-    fetch(url, {
-        method: 'GET'
-    }).then(function (res) {
-        res.json().then(function(data) {
-            console.log(data);
-        })
-    });
-}
+let url = 'http://155.138.134.91/queryAccountByID?idToQuery=jingleman';
+fetch(url, {
+    method: 'GET'
+}).then(function (res) {
+    res.json().then(function (data) {
+        console.log(data);
+    })
+});
 
-wrapper();
+//---------------------------------------CAMPAIGN TESTS------------------------------------------------
+await fetch('http://155.138.134.91/createCampaign', {
+    method: 'POST',
+    body: JSON.stringify({
+        id: 'jingleman',
+        name: 'Jingle Man',
+        email: 'jingle@sipher.co',
+        accountType: 'Personal'
+    }),
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+});
+
+let url = 'http://155.138.134.91/queryCampaignByID?idToQuery=torontomayoralelection';
+fetch(url, {
+    method: 'GET'
+}).then(function (res) {
+    res.json().then(function (data) {
+        console.log(data);
+    })
+});
+
+
