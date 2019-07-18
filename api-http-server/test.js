1239324952b8245baf773ec9d7f628788b9e1e70
kaii789 <kailong.huang@mail.utoronto.ca>
2019-07-18 12:04:53 -0400

    add createAccount response

--- api-http-server/test.js
+++ api-http-server/test.js
@@ -1,4 +1,5 @@
 // test script for HTTP server
+// Usage: enroll admin and add affiliation. Then run HTTPapi-test.js in the server and finally the fetch tests individually(so have the other fetch calls commented out).
 
 const fetch = require('node-fetch');
 
@@ -27,13 +28,20 @@ fetch(url, {
 });
 
 //---------------------------------------CAMPAIGN TESTS------------------------------------------------
+var start = Date.parse('2019-7-16');
+var end = Date.parse('2019-8-1');
+var startStr = start.toString();
+var endStr = end.toString();
+
 await fetch('http://155.138.134.91/createCampaign', {
     method: 'POST',
     body: JSON.stringify({
-        id: 'jingleman',
-        name: 'Jingle Man',
-        email: 'jingle@sipher.co',
-        accountType: 'Personal'
+        id: "torontomayoralelection",
+        name: "Toronto Mayoral Election",
+        campaignType: "Mayoral Election",
+        start: startStr,
+        end: endStr,
+        username: 'cityoftoronto'
     }),
     headers: {
         'Accept': 'application/json',
@@ -49,5 +57,3 @@ fetch(url, {
         console.log(data);
     })
 });
-
-
